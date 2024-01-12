25ff330c744c86097642682cb9fd7e8f49d2db9a
Linsiji <42350425+linsijiLaGuara@users.noreply.github.com>
2024-01-12 09:50:20 -0300

    add: header con titulo

--- adoptaunperrito/src/App.jsx
+++ adoptaunperrito/src/App.jsx
@@ -3,13 +3,14 @@ import MyCardPerritoBlanco from "./componentes/mycard/MyCardPerritoBlanco";
 import MyCardPerritoPrincesa from "./componentes/mycard/MyCardPerritoPrincesa";
 import MyCardPerritoPeque from "./componentes/mycard/MyCardPerritoPeque";
 import MyCardPerritoGrande from "./componentes/mycard/MyCardPerritoGrande";
-
+import Header from "./componentes/header/Header";
 
 import "./App.css";
 
 function App() {
   return (
     <>
+    <Header/>
       <div className='perritos-card'>
       <MyCardPerritoBlanco  />
         <MyCardPerritoPrincesa />
---
+++ adoptaunperrito/src/componentes/header/Header.jsx
@@ -0,0 +1,15 @@
+
+import './Header.css';
+
+
+const Header = () => {
+    const headerName = "Adopta un perrito"
+    return (
+    <div>
+   
+    <h1 className="titulo">{headerName}</h1>
+
+    </div>
+ );
+ };
+ export default Header;
